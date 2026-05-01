8ac6c8669ab140c06958829dacade6af7d6ab148
pondpal <kerstalm@outlook.com>
2026-05-01 15:12:57 -0700

    Add files via upload

--- pondpal/pages/_app.js
+++ pondpal/pages/_app.js
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="google-adsense-account" content="ca-pub-7893724198285520" />
-        <meta name="google-site-verification" content="0calb_heyG7VCzCnaSnXAQ3GVlJZoonOHaPa5qAZccQ" />
+        <meta name="google-site-verification" content="your-verification-code-here" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
