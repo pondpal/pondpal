f6d3e7cf7a5892563bec98cf519f2bdf0c0e3e5c
pondpal <kerstalm@outlook.com>
2026-05-20 16:39:14 -0700

    Update _app.js

--- pondpal/pages/_app.js
+++ pondpal/pages/_app.js
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="google-adsense-account" content="ca-pub-7893724198285520" />
         <meta name="google-site-verification" content="0calb_heyG7VCzCnaSnXAQ3GVlJZoonOHaPa5qAZccQ" />
+        <meta name="impact-site-verification" value="8cba4779-5d40-4ecf-875c-5265fd634872" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
