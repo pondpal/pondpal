fe1900ab207fe4fb56f8d9de598e2643538661c4
pondpal <kerstalm@outlook.com>
2026-07-18 13:28:45 -0700

    Update next.config.js

--- pondpal/next.config.js
+++ pondpal/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'pondpal.space' }],
+        destination: 'https://www.pondpal.space/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
-
 module.exports = nextConfig
